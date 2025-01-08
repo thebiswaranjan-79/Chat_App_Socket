2b96fc98865cd52c625bbb37f04ac2640b2731b8
Biswaranjan Mohanty <mbiswaranjan797@gmail.com>
2025-01-08 20:17:50 +0530

    Added the Basic Socket Connection

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 
 const app = express();
+const http = require("http");
+const socketio = require("socket.io");
+
 const dotenv = require("dotenv");
 dotenv.config();
 
+const server = http.createServer(app);
+const io = socketio(server);
+
+
 const PORT = process.env.PORT || 3000; // Use PORT from environment variables or default to 3000
 
-app.listen(PORT, () => {
-    console.log(`Server Started at PORT ${PORT}`);
+app.use("/", express.static(__dirname + "/public"));
+
+server.listen(PORT, () => {
+  console.log(`Server Started at PORT ${PORT}`);
 });
